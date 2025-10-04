e59692178528a5b00a7c87c922326a77eac5f7e1
3bdalrhmanS3d <abdalrhmansaad24@gmail.com>
2025-10-04 16:40:48 +0300

    edit urls

--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema')),
-    path('api/v1/', include('api.urls')),  # NOTE: uses your api app
+    path('api/v1/', include('api.urls')),
+    # redirect /api -> /api/docs
+    path('api/', RedirectView.as_view(url='/api/docs/', permanent=False)),
 ]
